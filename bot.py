c33fc55e7989efc508cb465c9758a5311fbcd508
wemmx <samshit74@gmail.com>
2025-11-09 18:31:59 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import telebot
+import random
+
+# ВСТАВЬ СЮДА СВОЙ НАСТОЯЩИЙ ТОКЕН!
+TOKEN = "8231460140:AAHQCV0G2oxG5NJDQrTl30PHpR9x6-j-J2U"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    welcome_text = """
+    🐉 Лаборатория Хротгара активна
+    Используй /quote для мудрости
+    """
+    bot.reply_to(message, welcome_text)
+
+@bot.message_handler(commands=['quote'])
+def send_quote(message):
+    quotes = [
+        "Л: Банальность зла — в его повседневности.",
+        "Партурнакс: Что есть голос, если не шепот ветра к времени?",
+        "Твой инсайт: Я — ружье, которое зарядил Чехов."
+    ]
+    chosen_quote = random.choice(quotes)
+    bot.reply_to(message, chosen_quote)
+
+print("Бот запущен...")
+bot.polling()
